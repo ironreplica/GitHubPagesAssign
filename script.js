924decb374d0a140a477367d597e681a7cfd0bb8
ironreplica <trevorchilds8@gmail.com>
2023-09-06 09:18:08 -0600

    Fixed Github Pages

---
+++ script.js
@@ -0,0 +1,6 @@
+function mouseOver(event){
+    event.target.innerHTML = "What did I say?";
+}
+function mouseLeave(event){
+    event.target.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quasi blanditiis obcaecati facere, sit voluptatum doloremque consequatur minus voluptatem quod nostrum ad, autem, ea suscipit!"
+}
